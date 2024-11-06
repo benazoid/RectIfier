aa9bebef3ede3c73e68c5418fccdab6d6b125945
Ben <benjamin.e.richards@gmail.com>
2024-11-06 11:57:56 -0500

    Added a AUTO_COLOR mode, where it makes the color the ave target color

--- src/main/java/org/yourcompany/yourproject/Rect.java
+++ src/main/java/org/yourcompany/yourproject/Rect.java
@@ -7,6 +7,8 @@ import java.awt.image.BufferedImage;
 
 public final class Rect {
 
+    final static boolean AUTO_COLOR = true;
+
     static MyCanvas mainCanvas;
     static int screenWidth;
     static int screenHeight;
@@ -40,6 +42,22 @@ public final class Rect {
         blue = b;
 
         intersectionPoly = getCanvasIntersection();
+    }
+
+    public Rect(int x, int y, int w, int h, double rot){
+        xPos = x;
+        yPos = y;
+        width = w;
+        height = h;
+        rotation = rot;
+
+        intersectionPoly = getCanvasIntersection();
+        
+        Color c = getAverageColor(intersectionPoly, mainCanvas);
+
+        red = c.getRed();
+        green = c.getGreen();
+        blue = c.getBlue();
     }
 
     public double getScore(BufferedImage targetImage, double[][] baseScoreArr){
@@ -85,11 +103,44 @@ public final class Rect {
 
         double rot  = getRandomInRange(rotation, 0, 2*Math.PI, mutationAmt);
 
+        if(AUTO_COLOR){
+            return new Rect(x, y, w, h, rot);
+        }
+
         int r = (int) getRandomInRange(red, 0, 255, mutationAmt);
         int g = (int) getRandomInRange(green, 0, 255, mutationAmt);
         int b = (int) getRandomInRange(blue, 0, 255, mutationAmt);
         
         return new Rect(x, y, w, h, rot, r, g, b);
+    }
+
+    public static Color getAverageColor(Polygon intersection, MyCanvas canvas){
+        Point topAndBottom = PolyStuff.getTopAndBottom(intersection);
+        PolyStuff.TBD[] lineTopsAndBottoms = PolyStuff.getTopBottomDirection(intersection);
+
+        float r = 0;
+        float g = 0;
+        float b = 0;
+
+        int ct = 0;
+
+        for(int y = topAndBottom.x; y < topAndBottom.y; y++){
+            Point startAndEnd = PolyStuff.getStartAndEnd(intersection, y, lineTopsAndBottoms);
+            for (int x = startAndEnd.x; x < startAndEnd.y; x++) {
+                Color c = new Color(canvas.getTargetImage().getRGB(x, y));
+                r += c.getRed();
+                g += c.getGreen();
+                b += c.getBlue();
+
+                ct++;
+            }
+        }
+
+        if (ct == 0) {
+            return new Color(0,0,0);
+        }
+
+        return new Color((int)(r/ct), (int)(g/ct), (int)(b/ct));
     }
 
     public Polygon getCanvasIntersection(){
@@ -134,7 +185,12 @@ public final class Rect {
         int y = (int)(Math.random() * screenHeight);
         int w = SIZE_MIN + (int)(Math.random() * (SIZE_MAX-SIZE_MIN));
         int h = SIZE_MIN + (int)(Math.random() * (SIZE_MAX-SIZE_MIN));
-        double rot = Math.random() * 2 * Math.PI; 
+        double rot = Math.random() * 2 * Math.PI;
+
+        if (AUTO_COLOR) {
+            return new Rect(x, y, w, h, rot);
+        }
+
         int r = (int)(Math.random() * 255);
         int g = (int)(Math.random() * 255);
         int b = (int)(Math.random() * 255);
